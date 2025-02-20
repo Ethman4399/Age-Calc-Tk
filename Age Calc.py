bf3dc01c7388878fae52e360b35b2aad1646dbf6
Ethan Stark <20184@jhc.school.nz>
2025-02-21 09:27:28 +1300

    Fixed the len() issue, was calling the wrong variables being d, m and y rather than day month and year. This would csue a int not Interable error and caue the calculator to not work. Also Added error prevention functions and a drop down for the months.

--- Age Calc.py
+++ Age Calc.py
@@ -6,21 +6,50 @@ import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
 # ___________________ FUNCTIONS _______________________
-def get_age():
+def validation():
 #gets the three entries (Date, Month and year)
-    d=int(e_date.get())
-    m=int(e_month.get())
-    y=int(e_year.get())
+#From get_age() function kept these inputs as strings to validate rather than converting
+    d= e_date.get()
+    y= e_year.get()
+    m = month_chosen.current()
+    msg = ''
+
+    if len(d) == 0 or len (y) == 0:
+      msg = 'day, month and year can\'t be empty'
+    else: 
+        try:
+            if any (ch.isdigit() for ch in d) == False:
+                msg = 'day must be a NUMBER'
+            elif m == 0:
+                msg = 'choose an appropriate MONTH'
+            elif any(ch.isdigit() for ch in y) == False:
+                msg = 'year must be a NUMBER'
+            else:
+                day = int(d)
+                month = m #month us already in number form list pos
+                year = int(y)
+                calc_age = find_age(day, month, year)
+                display_calc_age(calc_age)
+
+        except Exception as ep:
+            messagebox.showerror('error', ep)
+    if msg != '':
+            messagebox.showinfo('message', msg)
+
+def find_age(d, m, y):
     #find the age (difference between current and date of birth)
     age = today.year-y-((today.month, today.day)<(m,d))
-    tbox_age.config(state='normal')
+    return age
 
+def display_calc_age(age):
+    tbox_age.config(state='normal')
     #age calculated is inserted into the text box after clearing the previous info in the textbox
     tbox_age.delete('1.0', tk.END)
     tbox_age.insert(tk.END,age)
     tbox_age.config(state='disabled')
 
-
+def exit():
+    window.destroy()
 
 # ___________________  MAIN ________________________
 #Setting up the window
@@ -47,13 +76,31 @@ lb_year = tk.Label(window,text = "Year: ",font=('Arial',12,"bold"),fg="darkgreen
 
 #Input boxes for the date, month and year.
 e_date = tk.Entry(window,width=5)
-e_month = tk.Entry(window,width=5)
 e_year = tk.Entry(window,width=5)
 
+#combobox
+n = tk.StringVar()
+month_chosen = ttk.Combobox(window, textvariable = n, width=12)
+
+#adding combobox drop down menu
+month_chosen['values'] = ('Select a date...',
+                            'January',
+                            'February',
+                            'March',
+                            'April',
+                            'May',
+                            'June',
+                            'July',
+                            'August',
+                            'September',
+                            'October',
+                            'November',
+                            'December')
+
 #Button to calculate age
-btn_calculate_age = tk.Button(window,text="Calculate Age!",font=("Arial",13), command=get_age)
+btn_calculate_age = tk.Button(window,text="Calculate Age!",font=("Arial",13), command=validation)
 lb_calculated_age = tk.Label(window,text="The Calculated Age",font=('Arial',12,"bold"),fg="darkgreen",bg="#F7DC6F")
-tbox_age=tk.Text(window,width=10,height=1,state="disabled", bg="darkgreen",fg="white")
+tbox_age=tk.Text(window,width=10,height=1,state="disabled", bg="white",fg="black")
 btn_exit = tk.Button(window,text="Exit Application!",font=("Arial",13),command=exit)
 
 #This code places everything in the tk window using .place()
@@ -64,7 +111,7 @@ lb_date.place(x=450, y=100)
 lb_month.place(x=450, y=120)
 lb_year.place(x=450, y=140)
 e_date.place(x=510, y=100)
-e_month.place(x=510, y=120)
+month_chosen.place(x=510, y=120)
 e_year.place(x=510, y=140)
 btn_calculate_age.place(x=450,y=165)
 lb_calculated_age.place(x=400,y=200)
